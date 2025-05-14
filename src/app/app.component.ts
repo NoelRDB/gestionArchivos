0a22f8a3798a998df7374a3d2dbbc5be5260580b
NoelRDB <noelgijon98@gmail.com>
2025-05-14 10:49:49 +0200

    gestor de archivos funcional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FileRecord, FileService } from './services/file.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gestionArchivos';
+
+  filter: string = 'ALL';
+
+
+  constructor(private fileSvc: FileService)
+  {}
+
+  get archivoFiltrado(): FileRecord[]
+  {
+    const all = this.fileSvc.list();
+
+    if (this.filter === 'PENDING')
+    {
+      return all.filter (f => !f.processed)
+    };
+
+    if (this.filter === 'DONE')
+    {
+      return all.filter (f => f.processed)
+    };
+
+    return all;
+    }
+
+    add(file: FileRecord): void
+    {
+      this.fileSvc.addFile(file);
+    }
+
+    toggle(file: FileRecord): void
+    {
+      this.fileSvc.updateFile(file);
+    }
+
+    remove(file: FileRecord): void
+    {
+      this.fileSvc.deleteFile(file.id);
+    }
 }
